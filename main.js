d1ffa778fda5deefc54d9c36ead4555fff243203
Magda <jaskowska.magda@gmail.com>
2020-11-15 18:11:35 +0000

    add contact toggle

---
+++ main.js
@@ -0,0 +1,10 @@
+const kontaktSection = document.querySelector('.kontakt-section');
+
+function toggleKontakt(){
+    if (kontaktSection.style.display==="none"){
+        kontaktSection.style.display="block";
+    }
+    else {
+        kontaktSection.style.display="none";
+    }
+}

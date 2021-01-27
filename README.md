3d7ee9baa47271e4ec027d9a8230a232cc855d22
Magda <jaskowska.magda@gmail.com>
2021-01-27 14:44:53 +0000

    fix: files destructuration

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 #SANDAR
-Practice website
 
-HTML5 SCSS JavaScript
 
 This project will be updated along with my learning progress.
---
+++ src/main/main.js
@@ -0,0 +1,12 @@
+// contact toggle
+const contactSection = document.querySelector('.contact-section');
+
+function toggleContact() {
+    if (contactSection.style.display === "none") {
+        contactSection.style.display = "block";
+    }
+    else {
+        contactSection.style.display = "none";
+    }
+}
+
